7f4843396137d7dd329117c73f14251b513cc812
BlackSquirrelz <tobias.weisskopf.ch@gmail.com>
2020-03-29 13:55:47 +0200

    remove

--- mylog14Dashboard/dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='dashboard-home'),
-    path('population-chart/', views.population_chart, name='population-chart'),
-]
