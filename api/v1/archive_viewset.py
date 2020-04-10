7fc9057eaaa6db1f349757d35e257a09899e7a01
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2020-04-10 13:21:40 +0800

    Receive record zip by POST and extract to an UUID directory.
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

---
+++ api/v1/archive_viewset.py
@@ -0,0 +1,141 @@
+from datetime import datetime
+import logging
+import os
+import time
+import json
+import uuid
+import zipfile
+
+from celery import shared_task
+from django.shortcuts import get_object_or_404
+from django_celery_results.models import TaskResult
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+
+from applications.archives.models import Archive
+from applications.archives.serializer import ParserTaskSerializer
+from applications.archives.serializer import ArchiveSerializer
+#from applications.archives.serializer import ArchiveAnalysisSerializer
+#from applications.data_owners.models import DataOwner
+#from applications.data_owners.serializer import DataOwnerSerializer
+#from bitsocial_tasks.tasks import parse_archive
+
+logger = logging.getLogger(__name__)
+
+class ArchiveViewset(viewsets.ModelViewSet):
+    serializer_class = ArchiveSerializer
+
+    def get_queryset(self):
+        return Archive.objects.all()
+
+    def create_tmp_dir(self, public_key, file_obj):
+        dir_prefix = '/tmp'
+        dir_suffix = public_key
+        dir_path = os.path.join(dir_prefix, dir_suffix)
+        zipfile = os.path.join(dir_path, file_obj.name)
+        if not os.path.exists(dir_path):
+            try:
+                original_umask = os.umask(0)
+                os.makedirs(dir_path, 0o777)
+            finally:
+                os.umask(original_umask)
+        with open(zipfile, 'wb+') as dst:
+            logger.debug(type(file_obj))
+            logger.debug(file_obj.name)
+            logger.debug(file_obj.size)
+            if file_obj.multiple_chunks():
+                for chunk in file_obj.chunks():
+                    dst.write(chunk)
+            else:
+                dst.write(file_obj.read())
+        return dir_path, file_obj.name
+
+    def get_id_offset(self):
+        return int(round(time.time()*1000))*1000000
+
+    def create(self, request):
+        serializer = ArchiveSerializer(data=request.data)
+        #public_key = request.data['data_owner']
+        file_obj = request.data['file']
+        tmp_dir, file_name = self.create_tmp_dir(
+            'fake_public_key', file_obj
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            #archive_id = serializer.data['id']
+            #public_key = serializer.data['data_owner']
+            file_obj = request.data['file']
+            #t = parse_archive.delay(
+            #    tmp_dir,
+            #    file_name,
+            #    public_key,
+            #    request.META.get('HTTP_AUTHORIZATION').split(' ')[1],
+            #    self.get_id_offset(),
+            #)
+
+            #t = parse_archive.delay()
+            #for attempt in range(5):
+            #    print('Attempt #{}'.format(attempt + 1))
+            #    try:
+            #        task = TaskResult.objects.get(task_id=t.task_id)
+            #    except TaskResult.DoesNotExist:
+            #        time.sleep(0.1)
+            #        print('TaskResult does not exist yet ({}/5)'.format(attempt + 1))
+            #        continue
+            #    break
+            #task_serializer = ParserTaskSerializer(task)
+            #return Response(task_serializer.data, status.HTTP_201_CREATED)
+            filepath = os.path.join(tmp_dir, file_name)
+            unzip(filepath, tmp_dir)
+            return Response('got posted zip', status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors}, status.HTTP_400_BAD_REQUEST)
+
+
+def unzip(filepath, root_dirpath, target_dirname=''):
+    """Unzip file to root_dirpath/target_dirname/
+
+    Target_dirname is a random UUID by default, and user can specify it.
+    """
+    if target_dirname == '':
+        target_dirname = str(uuid.uuid4())
+    target_dirpath = os.path.join(root_dirpath, target_dirname)
+    print('FilePath: {}'.format(filepath))
+    print('TargetDir: {}'.format(target_dirpath))
+    with zipfile.ZipFile(filepath, 'r') as zip_ref:
+        zip_ref.extractall(target_dirpath)
+    os.remove(filepath)
+    return target_dirpath
+
+
+@shared_task(track_started=True)
+def parse_archive():
+    logger.info('Start unzipping temp zip')
+#def parse_archive(tmp_dir, file_name, public_key, api_key, offset):
+    #logger.info('Start unzipping temp zip')
+    #unzip_temp_zip(file_name, tmp_dir)
+    #remove_macosx_dir(tmp_dir)
+    #logger.info('Start sending files to S3')
+    #directory_to_s3(public_key, tmp_dir)
+
+    #tmp_json_dir = '{}-json'.format(tmp_dir)
+    #if not os.path.exists(tmp_json_dir):
+    #    os.makedirs(tmp_json_dir, 0o777)
+
+    #logger.info('Start parsing and posting')
+    #parser = Parser(tmp_dir, tmp_json_dir, public_key, api_key, offset)
+    #parser.parse()
+
+    #check_data_parsed(tmp_dir, tmp_json_dir, 'posts', 'posts')
+    #check_data_parsed(tmp_dir, tmp_json_dir, 'likes_and_reactions', 'reactions')
+
+    #remove_temp_dir(tmp_dir)
+    #remove_temp_dir(tmp_json_dir)
+    #logger.info('Data upload completed')
+
+    #sentiment_analyzer = SentimentAnalyzer(public_key)
+    #sentiment_analyzer.analyze()
+    #logger.info('Sentiment analysis completed')
---
+++ applications/archives/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.db import models
+
+def archive_object_key(instance, filename):
+    datetime_str = datetime.now().strftime('%Y/%m/%d/%H/%M/%S')
+    return '/'.join([
+        'fake_public_key', 'archives', datetime_str, filename
+    ])
+
+class Archive(models.Model):
+    #id = models.AutoField(primary_key=True)
+    file = models.FileField(
+        upload_to=archive_object_key, blank='false', null='false')
+    file_name = models.CharField(default='untitled_file', max_length=255)
+    file_size = models.BigIntegerField(default=0)
+    #uploaded_at = models.DateTimeField(auto_now_add=True)
+    #data_owner = models.ForeignKey(
+    #    'data_owners.DataOwner',
+    #    on_delete=models.CASCADE,
+    #    related_name='archives',
+    #)
