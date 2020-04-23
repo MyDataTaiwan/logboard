7f3915fb3559bc69a830a83f831c4966efb6c714
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2020-04-23 22:14:27 +0800

    Tiny updates.
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- applications/archives/admin.py
+++ applications/archives/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from applications.archives.models import Archive, Records
 
-# Register your models here.
+# Tell admin that the model object has an admin interface
+# (show in the admin webpage).
 admin.site.register(Records)
