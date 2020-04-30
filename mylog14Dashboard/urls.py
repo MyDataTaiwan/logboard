0b5c3b02f8a037210b0282bf44786309a167860f
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2020-04-30 18:06:34 +0800

    The media entrypoint should be accessible not only in debug mode.
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- mylog14Dashboard/urls.py
+++ mylog14Dashboard/urls.py
@@ -33,9 +33,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include(router.urls)),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.ADMIN_ENABLED:
     urlpatterns.append(path('admin/', admin.site.urls))
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
