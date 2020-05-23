8418cc4d4e8cc6c4c76a65a88816cb327945e6c1
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2020-05-23 15:19:13 +0800

    Support Japanese in template.
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- mylog14Dashboard/settings.py
+++ mylog14Dashboard/settings.py
@@ -175,6 +175,7 @@ from django.utils.translation import gettext_lazy as _
 LANGUAGES = (
     ('en', ('English')),
     ('zh-hant', _('Traditional Chinese')),
+    ('ja', _('Japanese')),
 )
 
 ## Where to store the locale files
