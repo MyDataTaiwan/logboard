1ce1d92bf2fd8d3d20e875d472f3fedfb0e7e0b7
Ubuntu <ubuntu@ip-172-31-41-39.ec2.internal>
2020-07-09 03:07:13 +0000

    Parse record content for response

--- api/v1/records/serializer.py
+++ api/v1/records/serializer.py
@@ -1,8 +1,22 @@
+import json
+import logging
 from rest_framework import serializers
 
 from applications.archives.models import Records
+
+
+logger = logging.getLogger(__name__)
+
 
 class RecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Records
         fields = '__all__'
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        try:
+            ret['content'] = json.loads(ret['content'].replace('\'', '\"').replace('None', 'null').replace('True', 'true').replace('False', 'false'))
+        except Exception as e:
+            logger.warning('Failed to parse content JSON. Reason: {}'.format(e))
+        return ret
