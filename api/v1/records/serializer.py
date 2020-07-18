2b3a61a636166b85541dc314e770158cd50744f4
James Chien <shc261392@gmail.com>
2020-07-18 11:10:45 +0800

    Remove deprecated files, Update requirements.txt

--- api/v1/records/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from applications.archives.models import Records
-
-class RecordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Records
-        fields = '__all__'
--- applications/archives/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ReviewHomeView, DashboardHomeView, DataView
-
-urlpatterns = [
-    path('', ReviewHomeView.as_view(), name='dashboard-home'),
-    path('<slug:userHash>/', DashboardHomeView.as_view(), name='dashboard-details'),
-    path('/line-chart/<slug:userHash>/', DataView.as_view(), name='line-chart'),
-    path('/location-mapping/', DataView.as_view(), name='location-mapping')
-]
